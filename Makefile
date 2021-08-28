0accf639e2ff097f394bca9d635f219f5d126b73
maximosan <maximosantoro@protonmail.com>
2021-08-28 14:43:55 -0300

    iteration 2 pgm

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 pbm:
-	convert ./iteration_1_samples/*.pbm ./iteration_1_samples/*.jpg
+	convert ./iteration_1_samples/pbm/*.pbm ./iteration_1_samples/jpg/*.jpg
+pgm:
+	convert ./iteration_2_samples/pgm/*.pgm ./iteration_2_samples/jpg/*.jpg
 	
-clean_jpg:
-	rm ./iteration_1_samples/*.jpg
+clean:
+	rm a.out
--- pgm.c
+++ pgm.c
@@ -1,16 +1,10 @@
-#include <math.h>
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
 
-    
-#define ANCHO 640
-#define ALTO 480
+#define ANCHO 1280
+#define ALTO 1000
 #define FOV 90
-#define CX  0.5
-#define CY  0.75
-#define CZ  1.9
-#define R   0.8
 
 #define G 9.81
 #define PI sqrt(G)
@@ -24,7 +18,7 @@ void line_inter(float p[3], const float o[3], const float d[3], float t);
 float sphere_distance(const float c[3], float r, const float o[3], const float d[3]);
 void sphere_normal(float normal[3], const float c[3], float r, const float p[3]);
 
-int get_intensity(const float o[3], const float d[3]);
+int get_intensity(const float o[3], const float d[3], int env_light);
 
 float dot_prod(const float a[3], const float b[3]) {
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
@@ -36,9 +30,9 @@ float norm(const float a[3]) {
 
 void normalize(float a[3]) {
     float n = norm(a);
-    a[0] = a[0] / n;
-    a[1] = a[1] / n;
-    a[2] = a[2] / n;
+    a[0] /= n;
+    a[1] /= n;
+    a[2] /= n;
 }
 
 void diff(float r[3], const float a[3], const float b[3]) {
@@ -54,12 +48,13 @@ void line_inter(float p[3], const float o[3], const float d[3], float t) {
     p[2] = o[2] + t * d[2];
 }
 
-#include <float.h>
-#define INFINITO FLT_MAX
+// #include <float.h>
+#define INFINITO 1e20
 
 float sphere_distance(const float c[3], float r, const float o[3], const float d[3]) {
     float co[3];
     diff(co, c, o);
+
     double cc = dot_prod(co, co);
     double b = dot_prod(co, d);
     double dist = b * b - cc + r * r;
@@ -69,10 +64,106 @@ float sphere_distance(const float c[3], float r, const float o[3], const float d
     
     dist = sqrt(dist);
 
-
+    if (b - dist > 0)
+        return b - dist;
+    
+    if (b + dist > 0)
+        return b + dist;
+    
+    return INFINITO;
 }
 
-void sphere_normal(float normal[3], const float c[3], float r, const float p[3]) {
 
+#define IA  5
+#define II  255
+#include <stdlib.h>
+#include <time.h>
+
+
+float luz[3] = {3, 2,  1};
+
+float spheres[][3] = {
+    {-.4, 0, 1.55},
+    {0, -.2, .7},
+    {.1, -.1, 4},
+    {.4, -.3, 5},
+    {-.9, .2, 1},
+    {-.1, .5, 2},
+};
+
+float radios[] = {
+    .2,
+    .25,
+    .11,
+    .9,
+    .48,
+    .5,
+};
+
+
+
+int get_intensity(const float o[3], const float d[3], int env_light) {
+
+    size_t mini = 0;
+    double mint = 1e20;
+
+    for(size_t i = 0; i < sizeof(spheres) / sizeof(spheres[0]); i++) {
+        double t = sphere_distance(spheres[i], radios[i], o, d);
+        if(t < mint) {
+            mini = i;
+            mint = t;
+        }
+    }
+
+    if(mint == 1e20)
+        return env_light;
+
+    float p[3];
+    line_inter(p, o, d, mint);
+
+    float n[3];
+    diff(n, p, spheres[mini]);
+    normalize(n);
+
+    int intensidad = env_light;
+
+    if(dot_prod(luz, n) > 0)
+        intensidad += fabs(dot_prod(luz, n)) * 255;
+
+    for(size_t i = 0; i < sizeof(spheres) / sizeof(spheres[0]); i++)
+        if(i != mini && sphere_distance(p, radios[i], spheres[i], n) < 1e20)
+            intensidad = env_light;
+
+    // intensidad += 5;
+
+    if(intensidad > 255)
+        intensidad = 255;
+
+    return intensidad;
+}
+
+
+int main(void) {
+
+    printf("P2\n");
+    printf("%d %d\n", ANCHO, ALTO);
+    printf("255\n");
+
+    normalize(luz);
+
+    for(int vy = ALTO / 2; vy > - ALTO / 2; vy--)
+        for(int vx = - ANCHO / 2; vx < ANCHO / 2; vx++) {
+            float d[3] = {
+                vx,
+                vy,
+                ANCHO / 2 / tan(FOV/ 2 * PI / 180)
+            };
+
+            normalize(d);
+            float origen[3] = {-.30, .20, -.71};
+            printf("%d\n", get_intensity(origen, d, 10));
+        }
+
+    return 0;
 }
 
