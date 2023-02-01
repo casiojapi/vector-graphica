d5ddaf16c49258f69a733cda9ee2ff667fb93412
maximo santoro <maximo.santoro@iovlabs.org>
2023-02-01 14:59:05 -0300

    fix V_SIZE() warning, use declares

--- common/define.h
+++ common/define.h
@@ -1,10 +1,10 @@
-#define ORIGIN -3.3
+#define ORIGIN -4.3
 
 #define STR_LENGTH 256
 
-#define WIDTH 1280
+#define WIDTH 2560
 #define HEIGHT 1280
-#define FOV 110
-#define N_LIGHTS 10
-#define N_SPHERES 7
-#define N_FILES 100
+#define FOV 90
+#define N_LIGHTS 6
+#define N_SPHERES 10
+#define N_FILES 10
